628da078ade7dac402b86e62f816ad9dcf7a3761
Vinay Singh <vinaysingh8866@gmail.com>
2021-07-19 13:08:46 -0400

    Done

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,53 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, Button } from 'react-native'
-
+import * as Google from 'expo-google-app-auth';
+import firebase from 'firebase';
 export class LoginScreen extends Component {
 
+    isUserEqual = (googleUser, firebaseUser) => {
+        if (firebaseUser) {
+            var providerData = firebaseUser.providerData;
+            for (var i = 0; i < providerData.length; i++) {
+                if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                    providerData[i].uid === googleUser.getBasicProfile().getId()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    onSignIn = (googleUser) => {
+        console.log('Google Auth Response', googleUser);
+        var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+            unsubscribe();
+            if (!this.isUserEqual(googleUser, firebaseUser)) {
+                var credential = firebase.auth.GoogleAuthProvider.credential(
+                    googleUser.idToken, googleUser.accessToken);
+                firebase.auth().signInWithCredential(credential)
+                    .then(res = (result) => {
+
+                        firebase.database()
+                            .ref('/users/' + result.user.uid)
+                            .set({
+                                gmail: result.user.email,
+                                profilePicture: result.additionalUserInfo.ptofile.picture
+                            })
+                    })
+                    .catch((error) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        var email = error.email;
+                        var credential = error.credential;
+                    });
+            
+                } else {
+
+                console.log('User already signed-in Firebase.');
+            
+            }
+        });
+    }
 
     signInWithGoogleAsync = async () => {
         try {
@@ -14,11 +59,17 @@ export class LoginScreen extends Component {
             });
 
             if (result.type === 'success') {
+
+
+                this.onSignIn(result)
+
+
                 return result.accessToken;
             } else {
                 return { cancelled: true };
             }
         } catch (e) {
+            alert(e)
             return { error: true };
         }
     }
